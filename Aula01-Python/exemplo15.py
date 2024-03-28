1f9009c371316a5491ca74886571ed01d82e8316
SERGIO GUSTAVO MEDINA PEREIRA <162819769+medinasergiogustavo@users.noreply.github.com>
2024-03-28 20:23:38 -0300

    Update exemplo15.py

--- Aula01-Python/exemplo15.py
+++ Aula01-Python/exemplo15.py
@@ -1,9 +1,10 @@
-print("Boa noite. Hoje é um bom dia para aprender Python")
+print("Bom dia. Hoje é um bom dia para aprender Python")
 print("")
 print("")
 print("Bem-vindo ao mundo do Python")
 print("")
 print("Vamos utilizar o VS-Code")
 print("")
-print("para as nossas aulas de Algoritmos e Logica de Programação")
+print("Para as nossas aulas de Algoritmos e Logica de Programação")
+
 
